cdfb9d80a446204a96b03e9380f87b66849c66a0
dmsalsgh97 <42495757+dmsalsgh97@users.noreply.github.com>
2020-03-02 12:56:55 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,16 @@
 #/bin/bash
 
 export PATH=/home/minho2/program/anaconda3/bin/python:$PATH
-export PATH=//home/minho2/.local/lib/pypy3.6:$PATH
 
 echo "input1 - input2 - output1 - output2 - interleaved"
 
 read input1 input2 output1 output2 interleaved
 
-pypy3 reform.py $input1 $output1 &
-pypy3 reform2.py $input2 $output2 &
+python reform.py $input1 $output1 &
+python reform.py $input2 $output2 &
 
-sleep 3
+sleep 100
 
 python interleaved.py $output1 $output2 $interleaved
 
 echo "all jobs finished"
-
